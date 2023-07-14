356e31dadf2ac579fe2f7aa7ac89d427ab81b60d
Timilehin Bello <emmatimmybello@gmail.com>
2023-07-14 16:55:09 +0100

    feat: made changes to smart contract to reduce gas fees

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -11,7 +11,6 @@ contract Vesting {
     }
 
     struct Stakeholder {
-        address stakeholderAddress;
         string stakeholderPost;
         uint256 vestingPeriod;
         uint256 startTime;
@@ -22,11 +21,32 @@ contract Vesting {
     mapping(address => Stakeholder) public stakeholders;
     mapping(address => bool) public whitelistedAddresses;
     mapping(address => Organisation) public organisations;
-    mapping(address => uint) public balance;
+    mapping(address => uint256) public balance;
 
     event CreateStakeholder(uint256 startTime, uint256 vestingPeriod);
 
-    function addOrganisation(string memory _name,address _orgAddress, uint256 _token) public {
+    modifier onlyOrganisationAdmin() {
+        require(
+            organisations[msg.sender].orgAddress == msg.sender,
+            "Unauthorized: Only the organization admin can perform this action"
+        );
+
+        _;
+    }
+
+    modifier onlyWhitelisted() {
+        require(
+            whitelistedAddresses[msg.sender],
+            "Only whitelisted address can withdraw"
+        );
+        _;
+    }
+
+    function addOrganisation(
+        string memory _name,
+        address _orgAddress,
+        uint256 _token
+    ) public {
         Organisation storage org = organisations[_orgAddress];
         org.orgAddress = _orgAddress;
         org.name = _name;
@@ -39,46 +59,55 @@ contract Vesting {
         string memory _post,
         uint256 _vestingPeriod,
         uint256 _token
-    ) public {
-        require(organisations[msg.sender].orgAddress == msg.sender, "Unauthorized");
-        require(organisations[msg.sender].token >= _token, "Token cannot be greater than the total token");
+    ) public onlyOrganisationAdmin {
+        Organisation storage org = organisations[msg.sender];
+        require(
+            org.token >= _token,
+            "Token cannot be greater than the total token"
+        );
 
-        stakeholders[_stakeholderAddress] = Stakeholder({
-            stakeholderAddress: _stakeholderAddress,
-            stakeholderPost: _post, 
-            vestingPeriod: _vestingPeriod,
-            startTime: block.timestamp,
-            token: _token,
-            claimedToken: 0
-        });
+        Stakeholder storage stakeholder = stakeholders[_stakeholderAddress];
+        stakeholder.stakeholderPost = _post;
+        stakeholder.vestingPeriod = _vestingPeriod;
+        stakeholder.startTime = block.timestamp;
+        stakeholder.token = _token;
+        stakeholder.claimedToken = 0;
 
         emit CreateStakeholder(block.timestamp, _vestingPeriod);
     }
 
-    function whitelistAddress(address _stakeholder) external {
-        require(organisations[msg.sender].orgAddress == msg.sender, "Only organization admin can perform this action");
+    function whitelistAddress(
+        address _stakeholder
+    ) external onlyOrganisationAdmin {
         whitelistedAddresses[_stakeholder] = true;
     }
 
-    function claimToken() external {
-        require(whitelistedAddresses[msg.sender], "Only whitelisted address can withdraw");
+    function claimToken() external onlyWhitelisted {
         Stakeholder storage stakeholder = stakeholders[msg.sender];
-        require(organisations[msg.sender].orgAddress == msg.sender || 
-        stakeholder.stakeholderAddress == msg.sender, "You do not belong to this organisation");
-        require(block.timestamp >= stakeholder.startTime + stakeholder.vestingPeriod, "Vesting period not yet over");
+        require(
+            stakeholder.startTime != 0,
+            "You do not belong to any organization"
+        );
+        require(
+            block.timestamp >=
+                stakeholder.startTime + stakeholder.vestingPeriod,
+            "Vesting period not yet over"
+        );
 
         uint256 claimableTokens = stakeholder.token - stakeholder.claimedToken;
         require(claimableTokens > 0, "No tokens available to claim");
 
-        stakeholder.claimedToken += claimableTokens; 
-        balance[stakeholder.stakeholderAddress] =  claimableTokens;
+        stakeholder.claimedToken += claimableTokens;
+        balance[msg.sender] += claimableTokens;
     }
-   
 
-    function getClaimedToken() public view returns (uint256){
+    function getClaimedToken() public view returns (uint256) {
         return balance[msg.sender];
     }
-    function getStakeholderPostion(address _address) public view returns (Stakeholder memory){
+
+    function getStakeholderPosition(
+        address _address
+    ) public view returns (Stakeholder memory) {
         return stakeholders[_address];
     }
 }
